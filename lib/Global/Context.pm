7c265bce9b6ad48e6b1f8e4f7db1d4be29c08e36
Ricardo Signes <rjbs@cpan.org>
2010-10-19 20:38:04 -0400

    exported symbol for $Context

--- lib/Global/Context.pm
+++ lib/Global/Context.pm
@@ -2,4 +2,27 @@ use strict;
 use warnings;
 package Global::Context;
 
+use Sub::Exporter -setup => {
+  exports    => [
+    init_context =>
+  ],
+  collectors => { '$Context' => \'_export_context_glob' },
+};
+
+our $Object;
+
+sub _export_context_glob {
+  my ($self, $value, $data) = @_;
+
+  $value  = 'Context' unless defined $value;
+  my $sym = "$data->{into}::$value";
+
+  {
+    no strict 'refs';
+    *{$sym} = *Global::Context::Object;
+  }
+
+  return 1;
+}
+
 1;
