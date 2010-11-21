30d42416b214d51944952c9e99b413bac0f193bb
Ricardo Signes <rjbs@cpan.org>
2010-11-21 16:53:03 -0500

    further refactor glob collector for spin-off

--- lib/Global/Context.pm
+++ lib/Global/Context.pm
@@ -7,65 +7,79 @@ use Global::Context::Env::Basic;
 use Global::Context::StackFrame::Trivial;
 use Sub::Exporter::Util ();
 
+{
+  package Sub::Exporter::GlobSharer;
+
+  sub glob_export_collector {
+    my ($default_name, $globref) = @_;
+
+    return sub {
+      my ($value, $data) = @_;
+
+      my $name;
+      $name = defined $value->{'-as'} ? $value->{'-as'} : $default_name;
+
+      my $sym = "$data->{into}::$name";
+
+      {
+        no strict 'refs';
+        *{$sym} = *$globref;
+      }
+
+      $_[0] = $globref;
+      return 1;
+    }
+  }
+}
+
 use Sub::Exporter -setup => {
   exports    => [
-    ctx_init => Sub::Exporter::Util::curry_method,
-    ctx_push => Sub::Exporter::Util::curry_method,
+    ctx_init => \'_build_ctx_init',
+    ctx_push => \'_build_ctx_push',
   ],
-  collectors => { '$Context' => \'_export_context_glob' },
+  collectors => {
+    '$Context' => Sub::Exporter::GlobSharer::glob_export_collector(
+      Context => \*Object,
+    )
+  },
 };
-
-sub default_variable_name { 'Context' }
-sub default_shared_glob   { \*Object }
-
-sub _export_context_glob {
-  my ($self, $value, $data) = @_;
-
-  my $name;
-  $name = defined $value->{'-as'}
-        ? $value->{'-as'}
-        : $self->default_variable_name;
-
-  $name =~ s/^\$//;
-
-  my $sym = "$data->{into}::$name";
-
-  {
-    no strict 'refs';
-    *{$sym} = *{ $self->default_shared_glob };
-  }
-
-  return 1;
-}
 
 sub default_context_class { 'Global::Context::Env::Basic' }
 sub default_frame_class   { 'Global::Context::StackFrame::Trivial' }
 
-sub ctx_init {
-  my ($class, $arg) = @_;
+sub _build_ctx_init {
+  my ($class, $name, $arg, $col) = @_;
 
-  my $ref = *{$class->default_shared_glob}{SCALAR};
-  Carp::confess("context has already been initialized") if $$ref;
+  return sub {
+    my ($arg) = @_;
 
-  $$ref = $class->default_context_class->new($arg)->with_pushed_frame(
-    $class->default_frame_class->new({
-      description => Carp::shortmess("context initialized"),
-      ephemeral   => 1,
-    }),
-  );
+    my $ref = *{ $col->{'$Context'} }{SCALAR};
+    Carp::confess("context has already been initialized") if $$ref;
 
-  return $$ref;
+    $$ref = $class->default_context_class->new($arg)->with_pushed_frame(
+      $class->default_frame_class->new({
+        description => Carp::shortmess("context initialized"),
+        ephemeral   => 1,
+      }),
+    );
+
+    return $$ref;
+  };
 }
 
-sub ctx_push {
-  my ($class, $frame) = @_;
+sub _build_ctx_push {
+  my ($class, $name, $arg, $col) = @_;
 
-  $frame = { description => $frame } unless ref $frame;
+  return sub {
+    my ($frame) = @_;
 
-  $frame = $class->default_frame_class->new($frame)
-    unless Scalar::Util::blessed($frame);
+    $frame = { description => $frame } unless ref $frame;
 
-  return ${ *{$class->default_shared_glob}{SCALAR} }->with_pushed_frame($frame);
+    $frame = $class->default_frame_class->new($frame)
+      unless Scalar::Util::blessed($frame);
+
+    return ${ *{ $col->{'$Context'} }{SCALAR} }->with_pushed_frame($frame);
+  }
 }
 
 1;
